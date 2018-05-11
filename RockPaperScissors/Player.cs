5cdc28954c850b7e021aba74771f087abb8ecf55
Anina Reinhart <reinharta@outlook.com>
2018-05-10 19:36:32 -0500

    Initial Commit

---
+++ RockPaperScissors/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public abstract class Player
+    {
+        public int turnNum = 0;
+        public int moveValue;
+        public int wins = 0;
+
+        public int MakeMove()
+        {
+            turnNum++;
+            return moveValue; 
+        }
+
+   
+    }
+}
---
+++ RockPaperScissors/PlayerCreation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class PlayerCreation
+    {
+        public string numOfPlayers;
+
+        public void GetNumPlayers()
+        {
+            Console.WriteLine("Choose how many players.\n-  1  OR  2  -");
+            numOfPlayers = Console.ReadLine();
+            AssignPlayers(numOfPlayers);
+        }
+
+        public void AssignPlayers(string numOfPlayers)
+        {
+            if (numOfPlayers == "1")
+            {
+                player1 =
+                object ComputerPlayer player2 = new ComputerPlayer();
+            }
+            if (numOfPlayers == "2")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("You entered an invalid selection. Please try again.");
+                GetNumPlayers();
+            }
+        }
+    }
+}
